8a7eba7bc5775d2f0b79dac54007421e9ae8633f
Jay R <ranajay5699@gmail.com>
2020-07-28 13:28:24 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import { AmplifyTheme } from 'aws-amplify-react';
+import ChatBot from './Chatbot/Chatbot'
+import awsconfig from './aws-exports';
 
-function App() {
+Amplify.configure(awsconfig);
+
+const App = () => {
+
+  useEffect(() => {
+    // if(getCookie("gibby") !== ""){
+    //   Interactions.send("RetUser")
+    // }
+  });
+
+  // Imported default theme can be customized by overloading attributes
+  const myTheme = {
+    ...AmplifyTheme,
+    sectionHeader: {
+      ...AmplifyTheme.sectionHeader,
+      backgroundColor: '#ff6600'
+    }
+  }
+
+  const setCookie = (name, value, exp) => {
+      var d = new Date(exp);
+      var expires = "expires="+ d.toUTCString();
+      document.cookie = name + "=" + JSON.stringify(value) + ";" + expires + ";path=/";
+  }
+
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+  const handleComplete = (err, confirmation) => {
+    if (err) {
+      alert('Bot conversation failed')
+      return;
+    }
+
+    if(confirmation.slots !== null && getCookie("gibby") === ""){
+      setCookie("gibby",confirmation.slots, confirmation.slots["targetDate"]);
+      alert("cookie set")
+    }
+    alert(JSON.stringify(confirmation));
+    return 'Done';
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <ChatBot
+          title="Prudential Bot"
+          theme={myTheme}
+          botName="RetUserTest_dev"
+          welcomeMessage="Welcome, how can I help you today?"
+          onComplete={handleComplete}
+          clearOnComplete={true}
+          conversationModeOn={false}
+        />
     </div>
   );
 }
